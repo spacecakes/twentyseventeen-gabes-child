e2f6e744734456d90160a6700d4e33069b712684
Gabriel <g.lundmark@gmail.com>
2018-04-25 16:16:22 +0200

    Initial commit

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<!-- Replaced site info with custom information -->
+	<a href="<?php echo esc_url(__('mailto:hello@gabriellundmark.com', 'Gabriel Lundmark')); ?>"><?php printf('© Gabriel Lundmark 2018'); ?></a>
+</div><!-- .site-info -->
